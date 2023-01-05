52b364d17c981c6ef6d689f32a1fbaa299aa62b9
yonatan921 <101233183+yonatan921@users.noreply.github.com>
2023-01-05 21:18:27 +0200

    Crate ServerFactory

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,8 +1,21 @@
 package bgu.spl.net.impl.stomp;
 
+import bgu.spl.net.impl.echo.EchoProtocol;
+import bgu.spl.net.impl.echo.LineMessageEncoderDecoder;
+import bgu.spl.net.srv.Server;
+import bgu.spl.net.srv.ServerFactory;
+
 public class StompServer {
 
     public static void main(String[] args) {
-        // TODO: implement this
+        int port = Integer.parseInt(args[0]);
+        int numberOfThreads = 3;
+        ServerFactory<String> stringServerFactory = new ServerFactory<>(numberOfThreads,
+                port,
+                EchoProtocol::new,
+                LineMessageEncoderDecoder::new,
+                true);
+        Server<String> server = stringServerFactory.getServer();
+        server.serve();
     }
 }
---
+++ server/src/main/java/bgu/spl/net/srv/ServerFactory.java
@@ -0,0 +1,34 @@
+package bgu.spl.net.srv;
+
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.api.MessagingProtocol;
+
+import java.util.function.Supplier;
+
+public class ServerFactory<T> {
+    private int port;
+    private int numThreads;
+    private Supplier<MessagingProtocol<T>> protocolFactory;
+    private Supplier<MessageEncoderDecoder<T>> readerFactory;
+    private boolean bool;
+    public ServerFactory(
+            int numThreads,
+            int port,
+            Supplier<MessagingProtocol<T>> protocolFactory,
+            Supplier<MessageEncoderDecoder<T>> readerFactory,
+            boolean bool){
+        this.port = port;
+        this.numThreads = numThreads;
+        this.protocolFactory = protocolFactory;
+        this.readerFactory = readerFactory;
+        this.bool = bool;
+    }
+
+    public Server<T> getServer(){
+        if (bool) return new Reactor<>(numThreads, port, protocolFactory, readerFactory);
+        else return Server.threadPerClient(port,protocolFactory, readerFactory);
+    }
+
+
+
+}
