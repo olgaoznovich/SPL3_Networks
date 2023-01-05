d873f87cac6cb3ed9ea302897271a4731318ef1b
yonatan921 <101233183+yonatan921@users.noreply.github.com>
2023-01-05 22:05:08 +0200

    Echo

--- server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
+++ server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
@@ -32,8 +32,10 @@ public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler
 
             while (!protocol.shouldTerminate() && connected && (read = in.read()) >= 0) {
                 T nextMessage = encdec.decodeNextByte((byte) read);
+                System.out.println(nextMessage);
                 if (nextMessage != null) {
                     T response = protocol.process(nextMessage);
+                    System.out.println(response);
                     if (response != null) {
                         out.write(encdec.encode(response));
                         out.flush();
