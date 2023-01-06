e90d0c3631cf9e07be275a2ce1ef717d3f835cee
ol-ozn <olgaoz@post.bgu.ac.il>
2023-01-06 13:11:55 +0200

    started protcol proccess

--- server/src/main/java/bgu/spl/net/impl/stomp/StompProtocol.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompProtocol.java
@@ -3,17 +3,26 @@ package bgu.spl.net.impl.stomp;
 import bgu.spl.net.api.*;
 import bgu.spl.net.srv.Connections;
 
-public class StompProtocol<T> implements StompMessagingProtocol<T> {
+public class StompProtocol implements StompMessagingProtocol<String> {
     private boolean shouldTerminate = false;
 
-    public void start(int connectionId, Connections<T> connections) {
+    public void start(int connectionId, Connections<String> connections) {
         //todo: implement
     }
     
-    public T process(T message) {
-        //todo: implement
+    public String process(String message) {
+        String[] msgComponents = message.split(System.lineSeparator());
+        switch(msgComponents[0]) {
+            case ("CONNECT"): {break;}
+            case ("SEND"): {break;}
+            case ("SUBSCRIBE"): {break;}
+            case ("UNSUBSCRIBE"): {break;}
+            case ("DISCONNECT"): {break;}
+            default: 
+        }
         return null;
     }
+
 	
 	/**
      * @return true if the connection should be terminated
