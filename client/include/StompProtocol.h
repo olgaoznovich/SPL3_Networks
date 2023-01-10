66e78adb53d307567c60bec72aceec6e9b16312d
ol-ozn <olgaoz@post.bgu.ac.il>
2023-01-10 17:55:42 +0200

    implemnted processLogin frame

--- client/include/StompProtocol.h
+++ client/include/StompProtocol.h
@@ -9,6 +9,14 @@ class StompProtocol
 {
 private:
     bool shouldTerminate;
+    std::vector<std::string> split(std::string s, char del);
+    std::string processLogin(std::vector<std::string> vec);
+    std::string processJoin(std::vector<std::string> vec);
+    std::string processExit(std::vector<std::string> vec);
+    std::string processReport(std::vector<std::string> vec);
+    std::string processSummary(std::vector<std::string> vec);
+    std::string processLogout(std::vector<std::string> vec);
+
 public:
     StompProtocol();
     std::string createFrame(std::string command);
--- client/src/StompProtocol.cpp
+++ client/src/StompProtocol.cpp
@@ -1,9 +1,32 @@
 #include "../include/StompProtocol.h"
+using namespace std;
 
 StompProtocol::StompProtocol() {}
 
 std::string StompProtocol::createFrame(std::string command) 
 {
+    //we assume the command input is legal
+    vector<string> strComps = split(command, ' ');
+    string keyword = strComps.at(0);
+    if(keyword == "login") 
+    {
+        processLogin(strComps);
+    } else if (keyword == "join")
+    {
+        processJoin(strComps);
+    } else if (keyword == "exit")
+    {
+
+    } else if (keyword == "report")
+    {
+
+    } else if (keyword == "summary")
+    {
+
+    } else if (keyword == "logout")
+    {
+
+    } 
 
 }
 
@@ -21,3 +44,51 @@ void StompProtocol::setShouldTerminate(bool value)
 {
     shouldTerminate = value;
 }   
+
+
+std::vector<std::string> StompProtocol::split(std::string s, char del)
+{
+    std::vector<std::string> vector;
+
+    std::stringstream ss(s);
+    std::string word;
+    while(!ss.eof()) 
+    {
+        std::getline(ss, word, del);
+        vector.push_back(word);
+    }
+
+    return vector;
+}
+
+string StompProtocol::processLogin(vector<string> vec)
+{
+    //build CONNECT frame
+    return "CONNECT\naccept-version:1.2\nhost:stomp.cs.bgu.ac.il\nlogin:" + vec.at(2) + "\npasscode:" + vec.at(3);
+} 
+
+string StompProtocol::processJoin(vector<string> vec)
+{
+    return "";
+} 
+
+string StompProtocol::processExit(vector<string> vec)
+{
+    return "";
+} 
+
+string StompProtocol::processReport(vector<string> vec)
+{
+    return "";
+} 
+
+string StompProtocol::processSummary(vector<string> vec)
+{
+    return "";
+} 
+
+string StompProtocol::processLogout(vector<string> vec)
+{
+    return "";
+} 
+
